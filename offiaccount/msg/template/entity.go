d66591194da7a0b174ea844e12c80c0caf2f6902
liujunren93 <liujunrenemail@qq.com>
2021-02-22 10:49:19 +0800

    dev

--- offiaccount/msg/template/entity.go
+++
@@ -1,6 +0,0 @@
-package template
-
-type sendRes struct {
-	ErrCode int32  `json:"errcode"`
-	ErrMsg  string `json:"errmsg"`
-}
--- offiaccount/token/entity.go
+++
@@ -1,7 +0,0 @@
-package token
-
-type accessTokenEntity struct {
-	AccessToken string `json:"access_token"`
-	ExpiresIn   int64  `json:"expires_in"`
-	CreateAt    int64
-}
